d977129f8928266145ff2e21eea6cc7298812125
Ivo Tonev <58517711+ivortonev@users.noreply.github.com>
2021-08-13 12:40:14 -0300

    Add files via upload

--- Fortigate/VPN/deps.sh
+++ Fortigate/VPN/deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in /usr/bin/mktemp /usr/bin/cat /usr/bin/grep /usr/bin/cut /usr/bin/date /usr/bin/ldapsearch /bin/rm /usr/bin/sort /usr/bin/uniq /usr/bin/echo /usr/bin/head /usr/bin/tail /usr/bin/tr ; do
+for i in /usr/bin/expect /usr/bin/mktemp /usr/bin/cat /usr/bin/grep /usr/bin/cut /usr/bin/date /usr/bin/ldapsearch /bin/rm /usr/bin/sort /usr/bin/uniq /usr/bin/echo /usr/bin/head /usr/bin/tail /usr/bin/tr ; do
 	if [ -f $i ] ; then
 		echo "$i ok"
 	else
