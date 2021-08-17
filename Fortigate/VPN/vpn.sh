60bba0d39af9961b8adf327851f8eaa0deab5d5e
Ivo Tonev <58517711+ivortonev@users.noreply.github.com>
2021-08-17 09:56:08 -0300

    Add files via upload

--- Fortigate/VPN/vpn.sh
+++ Fortigate/VPN/vpn.sh
@@ -3,7 +3,41 @@
 . ./conf.cfg
 . ./variaveis.cfg
 
-AD_USERNAME=$1
+# O nome de usuario no AD pode ser ate 20 caracteres
+AD_USERNAME=`$CMD_ECHO $1 | $CMD_TR [A-Z] [a-z] | $CMD_SED 's/[^0-9a-z]*//g' | $CMD_CUT -c 1-$MAX_CHARS`
+
+# Verificacao da data final
+#if [ $2 != "^[0-9]{4}/[0-9]{2}/[0-9]{2}$" ] ; then
+if [ $2 == "^202[1-9]/[0-3][0-9]/[0-9][0-9]$" ] ; then
+	$CMD_ECHO "Formato de data invalida. Informe ANO/MES/DIA"
+	exit 4
+fi
+
+DATE_END=$2
+
+VRF_ANO=`$CMD_ECHO $DATE_END | $CMD_CUT -f 1 -d "/"`
+VRF_MES=`$CMD_ECHO $DATE_END | $CMD_CUT -f 2 -d "/"`
+VRF_DIA=`$CMD_ECHO $DATE_END | $CMD_CUT -f 3 -d "/"`
+MAX_ANO=`$CMD_ECHO $DEF_DATE_END | $CMD_CUT -f 1 -d "/"`
+MAX_MES=`$CMD_ECHO $DEF_DATE_END | $CMD_CUT -f 2 -d "/"`
+MAX_DIA=`$CMD_ECHO $DEF_DATE_END | $CMD_CUT -f 3 -d "/"`
+
+if [ $VRF_ANO -gt $MAX_ANO ] ; then
+	echo "Data final incorreta - ano"
+	exit 3
+fi
+
+if [ $VRF_MES -gt $MAX_MES ] ; then
+	echo "Data final incorreta - mes"
+	exit 3
+fi
+
+if [ $VRF_DIA -gt $MAX_DIA ] ; then
+	echo "Data final incorreta - dia"
+	exit 3
+fi
+# Fim da verificacao da data final
+
 TMP_DIR=`$CMD_MKTEMP -d`
 
 VPN_USERNAME=`./$SCRIPT_AD_CHECK_LOGIN $AD_USERNAME`
@@ -41,9 +75,9 @@ FW_VPN_ADDRESS_NAME_DEFAULT="wks_$VPN_USERNAME"
 	$CMD_ECHO "Verificando data final da VPN ..."
 	./$SCRIPT_CFG_GET_SCHEDULE_EXTENDED $FW_HOST $FW_PORT $FW_USERNAME $FW_PASSWORD $FW_VPN_SCHEDULE_NAME_DEFAULT >> $TMP_DIR/schedule_extended
 	SCHEDULE_DATE_END=`$CMD_CAT $TMP_DIR/schedule_extended | $CMD_GREP "set end" | $CMD_CUT -f 2 -d "\"" | $CMD_TAIL -n 1 | $CMD_HEAD -n 1 | $CMD_CUT -f 2 -d ":" | $CMD_CUT -f 2 -d " " | $CMD_TR -d '[:cntrl:]' `
-	if [ "$SCHEDULE_DATE_END" != "$DEF_DATE_END" ] ; then
+	if [ "$SCHEDULE_DATE_END" != "$DATE_END" ] ; then
 		$CMD_ECHO "Alterando a data final da VPN ..."
-		./$SCRIPT_CFG_EDIT_SCHEDULE_DATE  $FW_HOST $FW_PORT $FW_USERNAME $FW_PASSWORD $FW_VPN_SCHEDULE_NAME_DEFAULT $DEF_DATE_START $DEF_DATE_END
+		./$SCRIPT_CFG_EDIT_SCHEDULE_DATE  $FW_HOST $FW_PORT $FW_USERNAME $FW_PASSWORD $FW_VPN_SCHEDULE_NAME_DEFAULT $DEF_DATE_START $DATE_END
 	fi
 		
 
